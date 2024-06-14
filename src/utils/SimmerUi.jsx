8b6b505b1c6e58ad55d9a9917a3a9df5438d7f27
nareshmitawa <naresh.mitawa@buzzclan.com>
2024-06-14 20:56:08 +0530

    Simmer Ui

--- src/utils/SimmerUi.jsx
+++ src/utils/SimmerUi.jsx
@@ -4,15 +4,29 @@ const SimmerUi = () => {
     return (
         <>
             <div className="card">
-                <div className="shimmerBG media"></div>
-                <div className="p-32">
-                    <div className="shimmerBG title-line"></div>
-                    <div className="shimmerBG title-line end"></div>
-                    <div className="shimmerBG content-line m-t-24"></div>
-                    <div className="shimmerBG content-line"></div>
-                    <div className="shimmerBG content-line"></div>
-                    <div className="shimmerBG content-line"></div>
-                    <div className="shimmerBG content-line end"></div>
+                <div className="col-lg-3 col-md-6 col-sm-12">
+                    <div className="shimmerBG media"></div>
+                    <div className="p-32">
+                        <div className="shimmerBG title-line"></div>
+                        <div className="shimmerBG title-line end"></div>
+                        <div className="shimmerBG content-line m-t-24"></div>
+                        <div className="shimmerBG content-line"></div>
+                        <div className="shimmerBG content-line"></div>
+                        <div className="shimmerBG content-line"></div>
+                        <div className="shimmerBG content-line end"></div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-12">
+                    <div className="shimmerBG media"></div>
+                    <div className="p-32">
+                        <div className="shimmerBG title-line"></div>
+                        <div className="shimmerBG title-line end"></div>
+                        <div className="shimmerBG content-line m-t-24"></div>
+                        <div className="shimmerBG content-line"></div>
+                        <div className="shimmerBG content-line"></div>
+                        <div className="shimmerBG content-line"></div>
+                        <div className="shimmerBG content-line end"></div>
+                    </div>
                 </div>
             </div>
         </>
