fba6f222246448eafa8cbf3cb5e06ed36369e177
nareshmitawa <naresh.mitawa@buzzclan.com>
2024-06-14 20:43:15 +0530

    simmer UI

--- src/components/Cards/ResCard.jsx
+++ src/components/Cards/ResCard.jsx
@@ -3,60 +3,67 @@ import { CDN_Link } from "../../utils/mockdata";
 import "./style.css";
 import PropTypes from 'prop-types';
 import { Button } from "react-bootstrap";
+import SimmerUi from "../../utils/SimmerUi";
 
-const ResCard = (props) => {
-    const [resData, setResData] = useState(props.ResData)
-
-
+const ResCard = ({ ResData }) => {
+    const [resData, setResData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const handleFilter = () => {
-        const filteredData = props.ResData.filter(
+        const filteredData = ResData.filter(
             (res) => res.info.avgRating > 4.5
         );
         setResData(filteredData);
-    }
+    };
+
+    const handleShowAll = () => {
+        setResData(ResData);
+    };
+
     useEffect(() => {
-        ApiFatch();
+        ApiFetch();
     }, []);
 
-    const ApiFatch = async() => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7046486&lng=76.71787259999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        
-        const json = await data.json();
+    const ApiFetch = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7046486&lng=76.71787259999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const json = await response.json();
+            setResData(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []);
+            setLoading(false);
+        } catch (error) {
+            console.error("Failed to fetch data", error);
+            setLoading(false);
+        }
+    };
 
-        setResData(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-        console.log("this is Swiggy Api",json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-        // setResData(json)
+    if (loading) {
+        return <SimmerUi />;
     }
 
-    console.log("this is log", props.ResData[0]);
     return (
         <>
             <div className="text-center w-100 my-3">
-                <Button className="btn-primary btn"
-                    onClick={handleFilter}
-                >Top Restorent</Button >
-                <Button className="btn-primary btn"
-                    onClick={() => {
-                        setResData(props.ResData)
-                    }}
-                >All Restorent</Button >
-            </div >
+                <Button className="btn btn-primary" onClick={handleFilter}>
+                    Top Restaurants
+                </Button>
+                <Button className="btn btn-primary" onClick={handleShowAll}>
+                    All Restaurants
+                </Button>
+            </div>
             <div className='container'>
                 <div className="all-Boxes" id="Bhoverbox">
                     <div className="Bhoverboxrow">
                         {resData.map((item, id) => (
                             <div className="col-lg-3 col-md-6 col-sm-12 mt-5" key={id}>
                                 <article className="card1">
-                                    <img className="card__image" src={CDN_Link + item.info.cloudinaryImageId} />
+                                    <img className="card__image" src={CDN_Link + item.info.cloudinaryImageId} alt={item.info.name} />
                                     <div className="card__data">
                                         <div className="card__info">
                                             <h2>{item.info.name}</h2>
                                             <p>{item.info.cuisines.join(", ")}</p>
                                         </div>
-                                        <h3 className="card__price">{item.info.cuisines.join(", ")}</h3>
                                         <h3 className="card__price">{item.info.avgRating}</h3>
-                                        {/* <button className="card__add">+</button> */}
                                     </div>
                                 </article>
                             </div>
@@ -65,13 +72,24 @@ const ResCard = (props) => {
                 </div>
             </div>
         </>
-    )
-}
-export default ResCard;
+    );
+};
 
 ResCard.propTypes = {
-    ResData: PropTypes.number.isRequired
-}
-ResCard.defaultProps = {
-    ResData: PropTypes.text
+    ResData: PropTypes.arrayOf(
+        PropTypes.shape({
+            info: PropTypes.shape({
+                name: PropTypes.string.isRequired,
+                cuisines: PropTypes.arrayOf(PropTypes.string).isRequired,
+                cloudinaryImageId: PropTypes.string.isRequired,
+                avgRating: PropTypes.number.isRequired,
+            }).isRequired,
+        })
+    ).isRequired,
 };
+
+ResCard.defaultProps = {
+    ResData: [],
+};
+
+export default ResCard;
---
+++ src/utils/SimmerUi.jsx
@@ -0,0 +1,21 @@
+import "./Simmer.css";
+
+const SimmerUi = () => {
+    return (
+        <>
+            <div className="card">
+                <div className="shimmerBG media"></div>
+                <div className="p-32">
+                    <div className="shimmerBG title-line"></div>
+                    <div className="shimmerBG title-line end"></div>
+                    <div className="shimmerBG content-line m-t-24"></div>
+                    <div className="shimmerBG content-line"></div>
+                    <div className="shimmerBG content-line"></div>
+                    <div className="shimmerBG content-line"></div>
+                    <div className="shimmerBG content-line end"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default SimmerUi;
